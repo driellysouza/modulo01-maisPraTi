79551d316b3432ccc567a295ded1f6a90eddec7f
Drielly <driellyalmeida18@gmail.com>
2024-08-29 00:35:43 -0300

    lessons

---
+++ lesson10.js
@@ -0,0 +1,22 @@
+/*let numberList = []
+
+numberList[0] = 10
+numberList[1] = 12
+numberList[2] = 0
+numberList[3] = 16
+numberList[4] = 7
+numberList[5] = 9
+
+console.log(numberList.sort((a, b) => a - b))*/
+
+function calculateLandArea(width, height) {
+    let area = width * height
+    return area
+}
+
+let width = 50
+let height = 100
+
+let area = calculateLandArea(width, height)
+
+console.log(`A área possui ${area} metros quadrados`)
